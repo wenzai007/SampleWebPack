5e38243c0e96040db979f276a1a92b6fd4f1bffa
Wenze Zhang <wenzzha@microsoft.com>
2020-02-17 21:15:40 +0800

    use react use Loader

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,9 +1,16 @@
 // Greeter.js
 
+import React, {Component} from 'react'
 var config = require('./config.json');
 
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = config.greetText;
-    return greet;
-  };
+class Greeter extends Component{
+    render(){
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter

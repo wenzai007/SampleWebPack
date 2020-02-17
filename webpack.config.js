40ba728992667daef792554b055b6c99f913b56d
Wenze Zhang <wenzzha@microsoft.com>
2020-02-17 22:23:56 +0800

    make the css a module by using the css loader and with options

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,12 @@ module.exports = {
                     {
                         loader: "style-loader"
                     }, {
-                        loader: "css-loader"
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]"
+                            }
+                        }
                     }
                 ]
             }

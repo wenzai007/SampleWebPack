5f6330cc80e436d4c2e8624cc22b9059b204191e
Wenze Zhang <wenzzha@microsoft.com>
2020-02-18 22:28:47 +0800

    add some more plugins of template plugin & hot load module

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
 
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
-      path: __dirname + "/public",//打包后的文件存放的地方
+      path: __dirname + "/build",//打包后的文件存放的地方
       filename: "bundle.js"//打包后输出文件的文件名
     },
 
@@ -19,12 +20,7 @@ module.exports = {
             {
                 test: /(\.jsx|\.js)$/,
                 use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "env", "react"
-                        ]
-                    }
+                    loader: "babel-loader"
                 },
                 exclude: /node_modules/
             },
@@ -50,6 +46,10 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.BannerPlugin('copyright can not be ruined.')
+        new webpack.BannerPlugin('copyright can not be ruined.'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin()//热加载插件
     ]
   }
